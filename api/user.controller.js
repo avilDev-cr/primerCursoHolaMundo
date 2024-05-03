8accdc2310f9b795e766bb52acebbda781c343c3
Andres Aviles <andrezavilesrdz@gmail.com>
2024-05-02 21:41:15 -0600

    Refactorizando los endpoint

--- api/user.controller.js
+++ api/user.controller.js
@@ -1,18 +1,33 @@
+const Users = require('./user')
 const User = {
-    get: (req, res)=>{
-        res.status(200).send('Este es un cliente')
+    get: async (req, res)=>{
+        const { id } = req.params
+        const user = await Users.findOne({_id: id})
+        res.status(200).send(user)
     },
-    list: (req,res) =>{
-        res.status(200).send('Hola Cliente')
+    list: async (req,res) =>{
+        const users = await Users.find()
+        res.status(200).send(users)
     },
-    create: (req, res)=> {
-        res.status(201).send('Creando cliente')
+    create: async (req, res)=> {
+        const user = new Users(req.body)
+        const savedUser = await user.save()
+        res.status(201).send(savedUser._id)
     },
-    update: (req,res) => {
-        res.status(204).send('Actualizando cliente')
+    update: async (req,res) => {
+        const { id } = req.params
+        const user = await Users.findOne({_id: id})
+        Object.assign(user, req.body)
+        await user.save()
+        res.sendStatus(204)
     },
-    destroy: (req, res) => {
-        res.status(204).send('Eliminando cliente')
+    destroy: async (req, res) => {
+        const { id } = req.params
+        const user = await Users.findOneAndDelete({_id: id})
+        if(user){
+            await user.deleteOne()
+        }
+        res.sendStatus(204)
     }
 }
 
